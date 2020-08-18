b5e79eaaec844449c5ad354b4cbbeb86cbdc0d03
katia kub <katiakub@gmail.com>
2020-08-18 14:52:44 +0300

    Validation

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -3,5 +3,6 @@
 require_relative 'codebreaker/guess_checker'
 require_relative 'codebreaker/game'
 require_relative 'codebreaker/loader'
+require_relative 'codebreaker/validation'
 require_relative 'codebreaker/version'
 require 'yaml'

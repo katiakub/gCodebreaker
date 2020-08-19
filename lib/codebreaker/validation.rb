d8662cb9180ec7cb6d94096a6cb964d91d43a902
katia kub <katiakub@gmail.com>
2020-08-19 12:01:55 +0300

    Validation for difficulties

--- lib/codebreaker/validation.rb
+++ lib/codebreaker/validation.rb
@@ -4,8 +4,15 @@ module Codebreaker
   module Validation
     MIN_NAME_LENGTH = 3
     MAX_NAME_LENGTH = 20
+
+    DIFFICULTIES = Loader.load('difficulties').keys
+
     def name_is_valid?(name)
       name.instance_of?(String) && name.length.between?(MIN_NAME_LENGTH, MAX_NAME_LENGTH)
+    end
+
+    def difficulty_is_valid?(difficulty)
+      DIFFICULTIES.include?(difficulty.to_sym)
     end
 
     def input_is_valid?(input)
